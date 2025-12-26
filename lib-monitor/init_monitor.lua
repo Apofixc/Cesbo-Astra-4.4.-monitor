a6642390769fbfd63defd9de78ad193a1b42c792
Apofixc <mihail.hodorovich@mail.ru>
2025-12-26 18:09:18 +0300

    fix

--- lib-monitor/init_monitor.lua
+++ lib-monitor/init_monitor.lua
@@ -8,9 +8,9 @@
 -- ===========================================================================
 
 require "./src/utils/logger"
-require "lib-monitor.src.utils.utils"
-require "lib-monitor.src.adapters.adapter"
-require "lib-monitor.src.channel.channel"
-require "lib-monitor.http.http_server"
-require "lib-monitor.src.dispatchers.channel_monitor_dispatcher"
-require "lib-monitor.src.dispatchers.dvb_monitor_dispatcher"
+require "./src/utils/utils"
+require "./src/adapters/adapter"
+require "./src/channel/channel"
+require "./http/http_server"
+require "./src/dispatchers/channel_monitor_dispatcher"
+require "./src/dispatchers/dvb_monitor_dispatcher"
