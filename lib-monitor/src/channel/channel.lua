3728f6610e7fa70e81b7c8121e3948bf9d2df77a
Apofixc <mihail.hodorovich@mail.ru>
2026-01-03 21:10:52 +0300

    Revert "fix resue"
    This reverts commit 5d0d55cf44a33a757876c9b516284c02346e1aef.

--- lib-monitor/src/channel/channel.lua
+++ lib-monitor/src/channel/channel.lua
@@ -46,13 +46,23 @@ local format_handlers = {
     end,
     udp = function(config)
         local cfg = {format = config.format}
-        cfg.addr = config.localaddr .. "@" .. config.addr .. ":" .. config.port
+        local localaddr = config.localaddr or ""
+        if localaddr ~= "" then
+            cfg.addr = localaddr .. "@" .. config.addr .. ":" .. config.port
+        else
+            cfg.addr = config.addr .. ":" .. config.port
+        end
         cfg.stream = get_stream(config.addr) or "unknown_stream"
         return cfg
     end,
     rtp = function(config)
         local cfg = {format = config.format}
-        cfg.addr = config.localaddr .. "@" .. config.addr .. ":" .. config.port
+        local localaddr = config.localaddr or ""
+        if localaddr ~= "" then
+            cfg.addr = localaddr .. "@" .. config.addr .. ":" .. config.port
+        else
+            cfg.addr = config.addr .. ":" .. config.port
+        end
         cfg.stream = get_stream(config.addr) or "unknown_stream"
         return cfg
     end,
