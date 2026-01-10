ed265a2e14e58d4dd2a6678b6a2a21cae91cd3a2
Apofixc <mihail.hodorovich@mail.ru>
2026-01-10 23:38:07 +0300

    fix

--- lib-monitor/src/adapters/dvb_tuner.lua
+++ lib-monitor/src/adapters/dvb_tuner.lua
@@ -237,8 +237,6 @@ function DvbTuner:_on_astra_data(data)
     end
 
     -- Оптимизированная проверка: сначала интервал, затем force или тяжелое условие
-
-    -- Оптимизированная проверка: сначала интервал, затем force или тяжелое условие
     if self:_should_send(self._astra_conf.time_check) and
        (self._force_timer >= self._force_interval or self._current_method(self._status, data, self._astra_conf.rate))
     then
