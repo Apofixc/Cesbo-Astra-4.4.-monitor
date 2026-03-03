b6228f592d9522a2e5c8cd43a9bed152baf9672c
Apofixc <mihail.hodorovich@mail.ru>
2026-03-03 20:24:57 +0300

    Refactor: Replace individual process kills with `killall` for `astra4.4.182` processes.

--- lua/start_test.sh
+++ lua/start_test.sh
@@ -10,12 +10,7 @@ case "$1" in
 	/opt/Cesbo-Astra-4.4.-monitor/astra4.4.182 /opt/Cesbo-Astra-4.4.-monitor/lua/varjag.lua &
 	;;
     stop)
-	kill `cat /var/run/test1.pid`
-	kill `cat /var/run/test2.pid`
-	kill `cat /var/run/test3.pid`
-	kill `cat /var/run/tv2.pid`
-	kill `cat /var/run/tv3.pid`
-	kill `cat /var/run/varjag.pid`
+	killall astra4.4.182
 	;;
 esac
 exit 0
